0970425be5b61c1f7429b416cf19ddc4347157b7
AmineHamzaoui443 <amine.hamzaoui@viacesi.fr>
2025-12-10 09:06:25 +0100

    CI-5.5

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,27 +1,37 @@
-import react from "eslint-plugin-react";
+import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
+
+  js.configs.recommended,   // configuration ESLint officielle
+
   {
     files: ["src/**/*.{js,jsx}"],
     ignores: ["build/**"],
+
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
-      ecmaFeatures: {
-        jsx: true // <-- Important pour parser le JSX
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true     // <-- active la syntaxe JSX
+        }
       }
     },
+
     plugins: {
-      react,
+      react: reactPlugin,
       "jsx-a11y": jsxA11y
     },
+
     rules: {
-      "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
-      "jsx-a11y/alt-text": "warn",
-      "no-unused-vars": "warn"
+      "react/prop-types": "off",
+      "no-unused-vars": "warn",
+      "jsx-a11y/alt-text": "warn"
     },
+
     settings: {
       react: {
         version: "detect"
