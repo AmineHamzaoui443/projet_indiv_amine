38d1e01b18140ce7fc64c6b661ca2e4a3a90ebc4
AmineHamzaoui443 <amine.hamzaoui@viacesi.fr>
2025-12-10 09:20:25 +0100

    CI-5.8

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,11 @@
+import js from "@eslint/js";
 import reactPlugin from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import js from "@eslint/js";
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   js.configs.recommended,
+
   {
     files: ["src/**/*.{js,jsx}"],
     ignores: ["build/**"],
@@ -12,29 +14,31 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
       parserOptions: {
-        ecmaFeatures: { jsx: true }
-      }
-    },
-
-    env: {
-      browser: true,
-      es2021: true
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        alert: "readonly",
+        FormData: "readonly",
+      },
     },
 
     plugins: {
       react: reactPlugin,
-      "jsx-a11y": jsxA11y
+      "jsx-a11y": jsxA11y,
     },
 
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
       "no-unused-vars": "warn",
-      "jsx-a11y/alt-text": "warn"
+      "jsx-a11y/alt-text": "warn",
     },
 
     settings: {
-      react: { version: "detect" }
-    }
-  }
+      react: { version: "detect" },
+    },
+  },
 ];
