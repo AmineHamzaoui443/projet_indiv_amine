9719f5277b6b2ccfde0a65d6703941ced319136e
AmineHamzaoui443 <amine.hamzaoui@viacesi.fr>
2025-12-18 16:09:36 +0100

    3rd

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test("affiche le titre 'Mon site d'annonces'", () => {
+test("affiche le titre 'la petite maison épouvante'", () => {
   render(<App />);
 
   const title = screen.getByText(/la petite maison épouvante/i);
