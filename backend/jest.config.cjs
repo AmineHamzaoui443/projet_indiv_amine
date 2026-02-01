33f6dbb01be12189b1903ce25c41c81de1b8cd04
AmineHamzaoui443 <medamine050420@gmail.com>
2026-02-01 20:43:55 +0100

    commit01/02

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,8 +1,14 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
+
+  // ✅ coverage native (évite babel-plugin-istanbul)
+  coverageProvider: 'v8',
   collectCoverage: true,
   collectCoverageFrom: ['controllers/**/*.js', 'middlewares/**/*.js'],
   coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text']
+  coverageReporters: ['lcov', 'text'],
+
+  // ✅ empêche toute transformation (donc pas Babel)
+  transform: {}
 };
